8abfae3eb36be59129ca7a051899a18a5096471b
hurmong <hurmong@hanmail.net>
2023-07-23 02:29:13 +0900

    commit

---
+++ static/js/slidar.js
@@ -0,0 +1,21 @@
+let currentSlide = 0;
+showSlide(currentSlide);
+
+function changeSlide(n) {
+  showSlide(currentSlide += n);
+}
+
+function showSlide(n) {
+  const slides = document.getElementsByClassName("slide");
+  if (n >= slides.length) {
+    currentSlide = 0;
+  } else if (n < 0) {
+    currentSlide = slides.length - 1;
+  }
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[currentSlide].style.display = "block";
+}
--- static/js/sub_bar.js
+++ static/js/sub_bar.js
@@ -1,6 +1,5 @@
 if(matchMedia("(max-width:576px)").matches)
 {
-    console.log('hello')
     $('.logo_img').attr("src","../static/img/logo_black.svg").fadeIn();
     $('.menu_bar').attr("style",'visibility : hidden');
     $('.direction img').attr("src","../static/img/mobile_dir.svg").fadeIn();
@@ -11,7 +10,6 @@ if(matchMedia("(max-width:576px)").matches)
 else
 {
     $(document).ready(function(){
-        console.log('hi');
         $('.menu_bar').attr("style",'visibility : visible');
         $('.ham-btn').attr("style",'visibility : hidden');
         $('.ham-con').attr("style",'visibility : hidden');
